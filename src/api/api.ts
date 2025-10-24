ef94c71d182c571a647b63fe1db6f4bcc719e09e
hsk-kr <hsk.coder@gmail.com>
2025-10-24 02:22:51 +0100

    chore: use axios
    fetch append base url by default at the end of the url for some reason

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,23 +1,25 @@
-const api = (pathname: string) => {
-  return fetch(`${import.meta.env.VITE_API_URL}${pathname}`);
-};
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 export const fetchPains = async () => {
   const res = await api("/pains");
 
-  if (!res.ok) {
+  if (res.status !== 200) {
     throw new Error("Failed to fetch");
   }
 
-  return res.json();
+  return res.data;
 };
 
 export const fetchPain = async (id: string) => {
   const res = await api(`/pains/${id}`);
 
-  if (!res.ok) {
+  if (res.status !== 200) {
     throw new Error("Failed to fetch");
   }
 
-  return res.json();
+  return res.data;
 };
