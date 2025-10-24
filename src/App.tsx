ee447e007d181431f5cde185696d34cb889ab333
hsk-kr <hsk.coder@gmail.com>
2025-10-24 02:27:43 +0100

    fix: set base path to react router

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,26 @@ import Template from "./components/Template";
 import PainList from "./components/PainList";
 import Pain from "./components/Pain";
 
-const router = createBrowserRouter([
+const BASE_PATH = import.meta.env.VITE_BASE_PATH || "/";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Template,
+      children: [
+        { index: true, Component: PainList },
+        {
+          path: "/:id",
+          Component: Pain,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    Component: Template,
-    children: [
-      { index: true, Component: PainList },
-      {
-        path: "/:id",
-        Component: Pain,
-      },
-    ],
+    basename: BASE_PATH,
   },
-]);
+);
 
 function App() {
   return <RouterProvider router={router} />;
